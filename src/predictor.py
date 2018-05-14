d17c0cb1a9c574a37020e2203339a74199906b28
Chogeumin <ed98b8ec97b0@gmail.com>
2018-05-14 11:26:06 +0900

    add predictor.py

--- src/predictor.py
+++ src/predictor.py
@@ -1,6 +1,20 @@
 from gensim.models import KeyedVectors
 
 class Predictor:
-    def __init__(self, std_id, lecture):
-        self.std_id = std_id
-        self.lecture = lecture
+    def __init__(self, model):
+        """
+        Init Model directory
+        """
+        self.model = KeyedVectors.load_word2vec_format(model, binary=True)
+    
+    def top_rank(self, lecture, threshold):
+        lecture_list = []
+        for lecture in self.model.most_similar(lecture):
+            if (lecture[1] >= threshold):
+                lecture_list.append(lecture)
+        return lecture_list
+
+    def predict(self, neighbors, threshold):
+        score = 0
+
+        return score
