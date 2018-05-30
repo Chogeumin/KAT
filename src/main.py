d5e265e7b32408d1a4a9f65f40e6d913f128773e
Chogeumin <ed98b8ec97b0@gmail.com>
2018-05-30 12:29:55 +0900

    delete unused code

--- src/main.py
+++ src/main.py
@@ -1,24 +1,22 @@
 from Lecture2Vec import Lecture2Vec
 from Predictor import Predictor
-from konlpy.tag import Twitter
-from konlpy.tag import Mecab
+# from konlpy.tag import Twitter
+# from konlpy.tag import Mecab
 import argparse, os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-b', "--build", type=bool, default=False, help="모델 제작 True/False")
-    parser.add_argument('-v', "--vocab", type=str, default="corpus/vocab", help="디렉토리 위치 corpus/<folder_name>")
-    parser.add_argument('-c', "--corpus", type=str, default="corpus/train", help="디렉토리 위치 corpus/<folder_name >")
-    parser.add_argument('-t', "--token", type=str, default="X", help="토크나이저 사용 여부 True/False")
-    parser.add_argument('-d', "--dist", type=bool, default=True, help="vocab, corpus 구분 True/False")
-    parser.add_argument('-p', "--pred", type=bool, default=True, help="예측기 사용 여부 True/False")
+    parser.add_argument('-b', "--build", type=bool, default=True, help="모델 제작 True/False")
+    parser.add_argument('-v', "--vocab", type=str, default="corpus", help="디렉토리 위치 corpus/<folder_name>")
+    parser.add_argument('-c', "--corpus", type=str, default="corpus", help="디렉토리 위치 corpus/<folder_name >")
+    # parser.add_argument('-t', "--token", type=str, default="X", help="토크나이저 사용 여부 True/False")
+    parser.add_argument('-d', "--dist", type=bool, default=False, help="vocab, corpus 구분 True/False")
+    parser.add_argument('-p', "--pred", type=bool, default=False, help="예측기 사용 여부 True/False")
     parser.add_argument('-n', '--name', type=str, default='data/word_vector.bin', help="워드벡터 저장 위치 data/<binary file name>.bin")
     args = parser.parse_args()
 
-    rows, columns = os.popen('stty size', 'r').read().split()
 
     print("\nparser statement")
-    print("-" * rows)
     print("build:\t", args.build)
     print("vocab:\t", args.vocab)
     print("corpus:\t", args.corpus)
@@ -44,27 +42,27 @@ if __name__ == "__main__":
         print("Predict lecture")
         pred = Predictor(name=args.name)
 
-        if (args.token == 'Twitter'):
-            t = Twitter()
-        elif (args.token == 'Mecab'):
-            t = Mecab()
-        else:
-            print("No tokenizer")
+        # if (args.token == 'Twitter'):
+        #     t = Twitter()
+        # elif (args.token == 'Mecab'):
+        #     t = Mecab()
+        # else:
+        #     print("No tokenizer")
 
-        infile = open('corpus/vocab/name.txt', 'r')
-        lectures = infile.readlines()
-        for lecture in lectures:
-            token = lecture
-            if (args.token == "Twitter" or args.token == "Mecab"):
-                print("Using tokenizer: ", args.token)
-                token = t.nouns(lecture)
-            else:
-                print("Using tokenizer: Mecab")
-                t = Mecab()
-                token = t.nouns(lecture)
-            print(lecture, " | ", token, "\n", "-" * 80)
-            if (len(token) != 0):
-                print(pred.top_rank(positive=token, negative="", threshold=0))
-            else:
-                print("한글 단어 없음.")
-            print("\n")
+        # infile = open('corpus/vocab/name.txt', 'r')
+        # lectures = infile.readlines()
+        # for lecture in lectures:
+        #     token = lecture
+        #     if (args.token == "Twitter" or args.token == "Mecab"):
+        #         print("Using tokenizer: ", args.token)
+        #         token = t.nouns(lecture)
+        #     else:
+        #         print("Using tokenizer: Mecab")
+        #         t = Mecab()
+        #         token = t.nouns(lecture)
+        #     print(lecture, " | ", token, "\n", "-" * 80)
+        #     if (len(token) != 0):
+        #         print(pred.top_rank(positive=token, negative="", threshold=0))
+        #     else:
+        #         print("한글 단어 없음.")
+        #     print("\n")
